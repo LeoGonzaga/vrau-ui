6efa7eda28444baad71e4acef4c47680199df1c6
LeoGonzaga <sacredlotus05@gmail.com>
2022-01-05 21:43:18 -0300

    iniciando estrutura do storybook

---
+++ src/stories/inputs/Inputs.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Input } from "./Input";
+
+export default {
+  title: "Example/Input",
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = { placeholder: "Digite seu email" };
